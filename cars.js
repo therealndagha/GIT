321d5f9ef0f366984db2fa2ce86ed1b9a5b146d2
Ndagha Kangoma <ndaghakangoma@gmail.com>
2025-05-30 07:45:58 +0200

    Add cars.js

---
+++ cars.js
@@ -0,0 +1,15 @@
+
+
+
+const cars = [
+    {
+        brand: 'toyota',
+        model: 'mark x',
+        yearOfMake: 2019
+    },
+    {
+        brand : 'nissan',
+        model: 'march',
+        yearOfMake: 2020
+    }
+]
