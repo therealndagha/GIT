c978962a5815b591b48f8848f641ae65c147ec8e
ndagha kangoma <ndaghakangoma@gmail.com>
2025-05-31 10:45:07 +0200

    Implement another important code

--- new_feature.js
+++ new_feature.js
@@ -8,5 +8,10 @@ console.log('i love git and github')
 
 /* implemented bug fix logic */
 
+<<<<<<< Updated upstream
 /* implemented another bug fix logic */
+=======
+/* more important code */
+
+>>>>>>> Stashed changes
 greetNewUsers('ndagha kangoma')
