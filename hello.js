3be59a2fd097874cb1ae7e2e702d69183cc248a5
ndagha kangoma <ndaghakangoma@gmail.com>
2025-05-31 10:19:45 +0200

    add another console.log

--- hello.js
+++ hello.js
@@ -3,6 +3,7 @@ console.log('Hello Git and github!!')
 console.log('Hello, Github!')
 console.log('Hello, adrian added this line')
 console.log('dev-adrian added this line also');
+console.log('bad code by dev-adrian');
 greetUsers('bossman')
 
 throwErrorNow()
