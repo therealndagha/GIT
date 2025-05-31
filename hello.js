ade74e498a68256c24bdda4cfc9202b3926f8881
ndagha kangoma <ndaghakangoma@gmail.com>
2025-05-31 10:17:05 +0200

    Add another log message in hello.js  by dev-adrian

--- hello.js
+++ hello.js
@@ -2,6 +2,7 @@
 console.log('Hello Git and github!!')
 console.log('Hello, Github!')
 console.log('Hello, adrian added this line')
+console.log('dev-adrian added this line also');
 greetUsers('bossman')
 
 throwErrorNow()
